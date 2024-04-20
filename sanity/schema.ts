57585251867eed2290cbf2d18da8e55447e8a9de
anotherITdude <abhi.baidya@gmail.com>
2024-04-20 12:09:14 +0200

    New: added schemas v1

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { feature } from './schemaTypes/feature'
+import { post } from './schemaTypes/post'
+import { author } from './schemaTypes/author'
+import { category } from './schemaTypes/category'
+import { blockContent } from './schemaTypes/blockContent'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [feature],
+  types: [post, author, category, blockContent, feature],
 }
