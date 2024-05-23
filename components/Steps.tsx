4d534bc73545747ec85f8c3fd2a9ddb5d8bde873
shohidul2015 <shd_islam@hotmail.com>
2024-05-23 10:37:03 +0200

    4 steps added

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -2,27 +2,35 @@ import React from "react";
 
 const Steps = () => {
   return (
-    <div className="flex my-4 bg-black text-center">
-      <div className="flex flex-col w-1/3 p-10 justify-center items-center">
-        <h2 className="text-white mb-3 ">Step 1</h2>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <div className="flex my-4 w-full bg-black text-center">
+      <div className="flex flex-col w-1/4 p-10 justify-center items-center">
+        <h2 className="text-white text-2xl ">Ports</h2>
+        <p className="text-white pt-8">
+        Administration of services for lower costs and increased capacity port side. Live tracking of vessel traffic, queueing system and push notifications for Just-in-Time benefits.
         </p>
       </div>
 
-      <div className="flex flex-col w-1/3 p-10 justify-center items-center">
-        <h2 className="text-white mb-3">Step 2</h2>
-        <p className="text-white ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <div className="flex flex-col w-1/4 p-10 justify-center items-center">
+        <h2 className="text-white text-2xl">Ships</h2>
+        <p className="text-white pt-8">
+            Digital logbooks, checklists and work-culture development on board. Leverage ship specific data for organizational gains and increased transparency.
         </p>
       </div>
 
-      <div className="flex flex-col w-1/3 p-10 justify-center items-center">
-        <h2 className="text-white mb-3">Step 3</h2>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <div className="flex flex-col w-1/4 p-10 justify-center items-center">
+        <h2 className="text-white text-2xl">Insurance</h2>
+        <p className="text-white pt-8">
+          Next level performance analysis and risk assessment through collected metrics. SMART effects with the FM8 platform as a regulatory instrument on shore and on board.
         </p>
       </div>
+
+      <div className="flex flex-col w-1/4 p-10 justify-center items-center">
+        <h2 className="text-white text-2xl">Compliance</h2>
+        <p className="text-white pt-8">
+          Benchmarking and rendering of reports to influence organizational short and long term sustainability strategies. Seamless compliance with international regulations.
+        </p>
+      </div>
+
     </div>
   );
 };
